f50a971526b2d3bd504de6c8e05cae2eaa87c493
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-27 15:33:31 +0200

    input editing

--- app/components/projects-list/component.js
+++ app/components/projects-list/component.js
@@ -3,6 +3,8 @@ const { Component, inject, computed } = Ember;
 
 export default Component.extend({
   tagName: '',
+  classNames: 'project-list',
+
   store: inject.service(),
 
   projects: computed(function() {
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('project', { path: 'projects/:project_id' });
 });
 
 export default Router;
