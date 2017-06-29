2bf2ec661a491fc6348a6eabd36b1f6c7bbefbf6
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-29 14:09:50 +0200

    open via editor feature

--- app/components/projects-list/project/menu/component.js
+++ app/components/projects-list/project/menu/component.js
@@ -3,12 +3,20 @@ const { Component, inject } = Ember;
 
 export default Component.extend({
   notify: inject.service(),
+  terminal: inject.service(),
+  ideName: 'code',
 
   actions: {
     remove() {
       this.get('project').destroyRecord().then(() => {
         this.get('notify').success('notify.projects.remove.success');
       });
+    },
+    
+    openInEditor() {
+      let projectPath = this.get('project.path')
+      let ideName = this.get('ideName');
+      return this.get('terminal').execute(`${ideName} ${projectPath}`);
     }
   }
 });
